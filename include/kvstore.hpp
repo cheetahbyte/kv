1fe9d9370051e45c03ec9d5c3f69338e75ee9be2
Leonhard Breuer <git@leonhardbreuer.de>
2026-04-24 09:06:47 +0200

    refactor(KVStore): make remove take `const std::string&` instead of `std::string`

--- include/kvstore.hpp
+++ include/kvstore.hpp
@@ -7,7 +7,7 @@ class KVStore {
     public:
         void put(const std::string&, const std::string&);
         std::optional<std::string> get(const std::string&) const;
-        void remove(std::string);
+        void remove(const std::string&);
     private:
         std::unordered_map<std::string, std::string> store;
 };
